f91d731b541a54f92abdb3d15eca702466f50505
Cerberus <1500575702@qq.com>
2021-10-19 23:58:58 +0800

    feat: build discard protocol and echo protocol server with netty

---
+++ src/main/java/icu/cerberus/nettydemo/discard/DiscardServerHandler.java
@@ -0,0 +1,29 @@
+package icu.cerberus.nettydemo.discard;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.ReferenceCountUtil;
+
+public class DiscardServerHandler extends ChannelHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()){
+//                System.out.println((char) in.readByte());
+                System.out.println(in.toString(io.netty.util.CharsetUtil.US_ASCII));
+                in.clear();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/test/java/icu/cerberus/nettydemo/NettydemoApplicationTests.java
@@ -0,0 +1,17 @@
+package icu.cerberus.nettydemo;
+
+import icu.cerberus.nettydemo.discard.DiscardServer;
+import icu.cerberus.nettydemo.echo.EchoServer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class NettydemoApplicationTests {
+
+    @Test
+    void contextLoads() throws Exception {
+        int port = 8000;
+        new EchoServer(port).run();
+    }
+
+}
